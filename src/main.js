452bebe75860090bac8bcc5ee365473fbf83253a
yigitden <yigitcandeniz@gmail.com>
2022-09-22 20:14:07 +0300

    pwa setting, added new icons

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import {fab} from '@fortawesome/free-brands-svg-icons'
 import {fas} from '@fortawesome/free-solid-svg-icons'
 import {far} from '@fortawesome/free-regular-svg-icons'
+import './registerServiceWorker'
 
 
 
