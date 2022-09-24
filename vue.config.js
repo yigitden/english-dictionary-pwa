973b70a2aa1effb8e636f22716c92324bba234b4
yigitden <yigitcandeniz@gmail.com>
2022-09-24 13:24:27 +0300

    manifest changed

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+   
+  pwa: {
+    name: "Pwa English Dictionary",
+    short_name: "ENG DICT",
+    iconPaths: 
+        {
+            faviconSVG: "img/icons/favicon.svg",
+            favicon32: "img/icons/favicon-32x32.png",
+            favicon16: "img/icons/favicon-16x16.png",
+            appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
+            maskIcon: "img/icons/safari-pinned-tab.svg",
+            msTileImage: "img/icons/msapplication-icon-144x144.png"
+          }
+    ,
+    
+    themeColor: "#f8fafc",
+    backgroundColor: "#f8fafc",
+    start_url: "/",
+    display: "fullscreen",
+    orientation: "portrait"
+  }
+}
